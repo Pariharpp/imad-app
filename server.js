ae36ec88ce8489abe570e917e1ce3b3ac12a21a9
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-02-24 17:50:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical-one',funnction(req,res){
+app.get('/artical-one',function(req,res){
     res.send('Artical no. 1');
 });
 app.get('/ui/style.css', function (req, res) {
