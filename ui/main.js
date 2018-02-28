3a67deb2784b0a97766437e4d1f0d89882feba6c
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-03-01 04:33:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,21 +2,7 @@
 var button= document.getElementById('counter');
 button.onclick = function(){
     // Create Request for counter end piont
-  /*  var request= new XMLHttpRequest();
-    
-    // capture the response  and store it 
-    request.onreadystatechange = function(){
-        if(request.readystate === XMLHttpRequest.DONE){
-            if(request.status===200){
-                var counter = request.reponseText;
-                var span = document.getElementById('count');
-                span.innerHTML= counter.toString();
-            }
-        }
-    };
-    //Make request 
-    request.open('GET',"http://pariharprahalad26.imad.hasura-app.io/counter", true);
-    request.send(null);*/
+  
 };
 //Submit button (name)
 var nameInput = document.getElementById('name');
