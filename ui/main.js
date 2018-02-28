3e4e6f31b2d7bbb05f21954f50695f8629ec3591
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-03-01 03:14:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 //counter code
 var button= document.getElementById('counter');
+var counter =0;
 button.onclick = function(){
     // Create Request for counter end piont
     var request= new XMLHttpRequest();
