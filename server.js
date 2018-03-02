42d324940d9fc40e24b505d0cdb5b70541862100
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-03-02 16:38:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var config={
     database:'pariharprahalad26',
     host:'db.imad.hasura-app.io',
     port:'5432',
-    password:process.env.DB_PASSWORD
+    password:'db-pariharprahalad26-37020'
 };
 var app = express();
 app.use(morgan('combined'));
