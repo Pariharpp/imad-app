1aafd222f34b9e25fe7576073fa82d3e26c9ebb0
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-03-10 15:22:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.get('/counter',function(req,res){
 
 function hash(input,salt){
     //create hash code
-    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 32, 'sha512');
     return hashed.toString('hex');
     
 }    
