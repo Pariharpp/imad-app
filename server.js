4d30548f7cb1b175e1d605098b76af3ff51be8fa
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-03-10 17:17:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ function hash(input,salt){
     
 }    
 app.get('/hash/:input',function(req,res){
-    var hashedString = hash(req.params.input,'random-string-prahalad-parihar');
+    var hashedString = hash(req.params.input,'');
     res.send(hashedString);
 });
 var Pool= new Pool(config);
