23666443142e7f0b99c23b6bc6faa2d9d5a3b4f1
Pariharpp <36411279+Pariharpp@users.noreply.github.com>
2018-03-10 15:07:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ function hash(input,salt){
 app.get('/hash/:input',function(req,res){
     var hashedString = hash(req.params.input,'random-String');
     res.send(hashedString);
-}) ;   
+});   
     
     
 });
